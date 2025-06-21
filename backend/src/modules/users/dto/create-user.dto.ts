731916faf1eb8c56db2e88b259b179401933ae40
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-06-21 11:52:59 -0300

    feat: adiciona DTOs para criação e atualização de usuários com validações

---
+++ backend/src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Match } from 'src/common/decorators/match.decorator';
+
+export class CreateUserDto {
+  @IsString({ message: 'O nome deve ser um texto.' })
+  @IsNotEmpty({ message: 'O nome é obrigatório.' })
+  @MinLength(3, { message: 'O nome deve ter pelo menos 3 caracteres.' })
+  @MaxLength(255, { message: 'O nome deve ter no máximo 255 caracteres.' })
+  name: string;
+
+  @IsEmail({}, { message: 'Informe um e-mail válido.' })
+  @IsNotEmpty({ message: 'O e-mail é obrigatório.' })
+  @MaxLength(255, { message: 'O e-mail deve ter no máximo 255 caracteres.' })
+  email: string;
+
+  @IsString({ message: 'A senha deve ser um texto.' })
+  @IsNotEmpty({ message: 'A senha é obrigatória.' })
+  @MinLength(6, { message: 'A senha deve ter pelo menos 6 caracteres.' })
+  @MaxLength(60, { message: 'A senha deve ter no máximo 60 caracteres.' })
+  password: string;
+
+  @Match('password', {
+    message: 'As senhas não coincidem.',
+  })
+  confirmPassword: string;
+}
---
+++ backend/src/modules/users/dto/update-user.dto.ts
@@ -0,0 +1,10 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
+import { ValidateIf } from 'class-validator';
+import { Match } from 'src/common/decorators/match.decorator';
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ValidateIf((values: UpdateUserDto) => values.password !== undefined)
+  @Match('password', { message: 'As senhas não coincidem.' })
+  confirmPassword: string;
+}
