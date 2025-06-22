5a01a287beb01c96fa2f96810d3b180399ef4989
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-06-22 18:02:26 -0300

    feat: adiciona serviço de hashing com bcrypt e classe abstrata para hashing

---
+++ backend/src/modules/auth/hashing/bcrypt.service.ts
@@ -0,0 +1,13 @@
+import { HashingService } from 'src/modules/auth/hashing/hashing.service';
+import * as bcrypt from 'bcryptjs';
+
+export class BcryptService extends HashingService {
+  async hash(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt();
+    return bcrypt.hash(password, salt);
+  }
+
+  compare(password: string, passwordHash: string): Promise<boolean> {
+    return bcrypt.compare(password, passwordHash);
+  }
+}
