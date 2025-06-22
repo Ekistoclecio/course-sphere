a02bd303bdb7beb69da25db5a8a407c16ae8a731
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-06-22 11:18:27 -0300

    feat: adiciona UsersModule às dependências do módulo de cursos

--- backend/src/modules/courses/courses.module.ts
+++ backend/src/modules/courses/courses.module.ts
@@ -3,9 +3,10 @@ import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
 import { Course } from 'src/modules/courses/entities/course.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from 'src/modules/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course])],
+  imports: [TypeOrmModule.forFeature([Course]), UsersModule],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
