313543c38378d5f21304c8cf71bbdca8bf89c373
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-06-22 01:32:55 -0300

    feat: adiciona módulo Lessons ao módulo principal da aplicação

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'src/modules/users/users.module';
 import { AppDataSource } from 'src/config/db/data-source';
 import { CoursesModule } from 'src/modules/courses/courses.module';
+import { LessonsModule } from 'src/modules/lessons/lessons.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(AppDataSource.options),
     UsersModule,
     CoursesModule,
+    LessonsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
