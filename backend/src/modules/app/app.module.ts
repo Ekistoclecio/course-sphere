12f372a12d49688021b8e1d4945895ed60af48e7
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-06-22 12:10:18 -0300

    feat: configura para a aplicação usar variáveis de ambiente

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -6,9 +6,11 @@ import { UsersModule } from 'src/modules/users/users.module';
 import { AppDataSource } from 'src/config/db/data-source';
 import { CoursesModule } from 'src/modules/courses/courses.module';
 import { LessonsModule } from 'src/modules/lessons/lessons.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot(AppDataSource.options),
     UsersModule,
     CoursesModule,
