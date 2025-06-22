4ca5db89817a4cfc10648f34e4a61826379e60a1
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-06-22 00:39:49 -0300

    feat: adiciona módulos de usuários e cursos ao módulo principal da aplicação

--- backend/src/modules/app/app.module.ts
+++ backend/src/modules/app/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from 'src/modules/users/users.module';
 import { AppDataSource } from 'src/config/db/data-source';
+import { CoursesModule } from 'src/modules/courses/courses.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(AppDataSource.options)],
+  imports: [
+    TypeOrmModule.forRoot(AppDataSource.options),
+    UsersModule,
+    CoursesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
