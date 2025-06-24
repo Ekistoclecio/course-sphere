1cbc7d1afab8046a47cf9a3aed8c3386e418ca31
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-06-24 14:37:00 -0300

    refactor(frontend): altera nome do campo de confirmação de senha no esquema de validação de cadastro para 'confirm_password'

--- frontend/src/schemas/user/signup.ts
+++ frontend/src/schemas/user/signup.ts
@@ -7,17 +7,17 @@ export const signUpBaseSchema = z.object({
     .string()
     .min(6, 'A senha deve ter pelo menos 6 caracteres.')
     .nonempty('A senha é obrigatória.'),
-  confirmPassword: z
+  confirm_password: z
     .string()
     .min(6, 'A senha deve ter pelo menos 6 caracteres.')
     .nonempty('A senha é obrigatória.'),
 });
 
 export const signUpSchema = signUpBaseSchema.refine(
-  (data) => data.password === data.confirmPassword,
+  (data) => data.password === data.confirm_password,
   {
     message: 'As senha e confirmação de senha não coincidem.',
-    path: ['confirmPassword'],
+    path: ['confirm_password'],
   }
 );
 
