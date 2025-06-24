9939767f88dc57127ea3b331036d8aa8ee708b9d
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-06-24 11:01:52 -0300

    refactor(frontend): altera nome do arquivo de apiService para client.ts

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth, { NextAuthOptions, User } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { jwtDecode } from 'jwt-decode';
-import { ApiServiceInstance } from '@/services';
+import { ApiServiceInstance } from '@/services/client';
 
 export interface LoginTokenDecoded {
   id: number;
