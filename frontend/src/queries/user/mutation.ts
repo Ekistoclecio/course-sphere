13b4b71f49c413e631878e2e2549d50950cbdbe9
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-06-24 22:10:12 -0300

    refactor(frontend): substitui chamadas de serviço de usuário por métodos do modelo UserModel para melhor organização e manutenção do código

--- frontend/src/queries/user/mutation.ts
+++ frontend/src/queries/user/mutation.ts
@@ -1,17 +1,17 @@
 import { useMutation } from '@/queries/adapters';
 import { EditProfileData } from '@/schemas/user/profileEdit';
-import { userService } from '@/services/user';
+import { UserModel } from '@/models/user';
 
 export const useCreateUser = () => {
-  return useMutation(userService.create);
+  return useMutation(UserModel.create);
 };
 
 export const useUpdateUser = () => {
   return useMutation(({ id, user }: { id: number; user: Partial<EditProfileData> }) =>
-    userService.update(user, id)
+    UserModel.update(user, id)
   );
 };
 
 export const useDeleteUser = () => {
-  return useMutation(userService.remove);
+  return useMutation(UserModel.remove);
 };
