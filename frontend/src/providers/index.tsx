a0e9257763c006b6d355b734b1c06f25d96275ac
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-06-25 10:07:18 -0300

    feat(frontend): integra o provedor de localização para suporte a datas em português

--- frontend/src/providers/index.tsx
+++ frontend/src/providers/index.tsx
@@ -9,6 +9,12 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import 'dayjs/locale/pt-br';
+import dayjs from 'dayjs';
+
+dayjs.locale('pt-br');
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
   return (
@@ -19,7 +25,11 @@ export function AppProviders({ children }: { children: React.ReactNode }) {
         <SessionProvider>
           <QueryClientProvider client={queryClient}>
             <ReactQueryDevtools initialIsOpen={false} />
-            <ConfiguredSnackbarProvider>{children}</ConfiguredSnackbarProvider>
+            <ConfiguredSnackbarProvider>
+              <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br">
+                {children}
+              </LocalizationProvider>
+            </ConfiguredSnackbarProvider>
           </QueryClientProvider>
         </SessionProvider>
       </ThemeProvider>
