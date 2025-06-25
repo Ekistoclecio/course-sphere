3533f96de2a32ae1a3572d5c932ed5f4e5da9e7e
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-06-25 16:28:39 -0300

    feat(backend): adiciona validação para o ID do curso no DTO de busca de aulas

--- backend/src/modules/lessons/dto/find-lesson.det.ts
+++ backend/src/modules/lessons/dto/find-lesson.det.ts
@@ -1,8 +1,20 @@
-import { IsEnum, IsOptional, IsString } from 'class-validator';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { LessonStatus } from 'src/modules/lessons/entities/lesson.entity';
+import { Transform } from 'class-transformer';
 
 export class FindLessonsDto extends PaginationDto {
+  @IsNotEmpty({ message: 'O ID do curso é obrigatório.' })
+  @IsNumber({}, { message: 'O ID do curso deve ser um número.' })
+  @Transform(({ value }) => Number(value))
+  course_id: number;
+
   @IsOptional()
   @IsString({ message: 'O campo de busca deve ser um texto válido.' })
   search?: string;
