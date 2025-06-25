b5d0f87ec1a592e38084f8cd93f6ae39f10797f3
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-06-25 10:06:05 -0300

    feat(frontend): implementa modal para criação de cursos com validação de formulário e gerenciamento de estado

---
+++ frontend/src/components/organisms/CreateCourseModal/index.tsx
@@ -0,0 +1,121 @@
+'use client';
+
+import * as React from 'react';
+import { Button, TextField, Box, Typography } from '@mui/material';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { Controller } from 'react-hook-form';
+
+import { CustomModal } from '@/components/molecules/CustomModal';
+import { useCreateCourseModal } from '@/components/organisms/CreateCourseModal/useCreateCourseModal';
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
+import dayjs from 'dayjs';
+import { Course } from '@/schemas/course/course';
+import { InfoNotice } from '@/components/atoms/InfoNotice';
+
+type CreateCourseModalProps = {
+  open: boolean;
+  onClose: () => void;
+  onCreateSuccessCallback?: (createdCourse: Course) => void;
+};
+
+export const CreateCourseModal = ({
+  open,
+  onClose,
+  onCreateSuccessCallback,
+}: CreateCourseModalProps) => {
+  const { register, handleSubmit, control, errors, isSubmitting, handleFormSubmit } =
+    useCreateCourseModal({ onClose, onCreateSuccessCallback });
+
+  return (
+    <CustomModal open={open} onClose={onClose} width={600}>
+      <CustomModal.Header>
+        <Box display="flex" alignItems="center" gap={1}>
+          <LibraryAddIcon sx={{ fontSize: 24 }} color="primary" />
+          <Typography variant="h4">Criar curso</Typography>
+        </Box>
+      </CustomModal.Header>
+
+      <CustomModal.Content>
+        <Box component="form" display="flex" flexDirection="column">
+          <TextField
+            label="Nome"
+            type="text"
+            fullWidth
+            margin="normal"
+            {...register('name')}
+            error={!!errors.name}
+            helperText={errors.name?.message}
+          />
+
+          <TextField
+            label="Descrição"
+            type="text"
+            fullWidth
+            margin="normal"
+            multiline
+            rows={4}
+            {...register('description')}
+            error={!!errors.description}
+            helperText={errors.description?.message}
+          />
+
+          <Controller
+            name="start_date"
+            control={control}
+            render={({ field }) => (
+              <DatePicker
+                label="Data de início"
+                value={field.value ? dayjs(field.value) : null}
+                onChange={(date) => field.onChange(date?.toISOString())}
+                slotProps={{
+                  textField: {
+                    fullWidth: true,
+                    margin: 'normal',
+                    error: !!errors.start_date,
+                    helperText: errors.start_date?.message,
+                  },
+                }}
+              />
+            )}
+          />
+
+          <Box>
+            <Controller
+              name="end_date"
+              control={control}
+              render={({ field }) => (
+                <DatePicker
+                  label="Data de término"
+                  value={field.value ? dayjs(field.value) : null}
+                  onChange={(date) => field.onChange(date?.toISOString())}
+                  slotProps={{
+                    textField: {
+                      fullWidth: true,
+                      margin: 'normal',
+                      error: !!errors.end_date,
+                      helperText: errors.end_date?.message,
+                    },
+                  }}
+                />
+              )}
+            />
+            <InfoNotice message="Apos a data de término, todas a as aula criadas dentro desse curso serão automaticamente marcadas como arquivadas pelo sistema." />
+          </Box>
+        </Box>
+      </CustomModal.Content>
+
+      <CustomModal.Footer align="right">
+        <Button onClick={onClose} disabled={isSubmitting} sx={{ color: 'text.primary' }}>
+          Cancelar
+        </Button>
+        <Button
+          variant="contained"
+          onClick={handleSubmit(handleFormSubmit)}
+          disabled={isSubmitting}
+        >
+          Salvar
+        </Button>
+      </CustomModal.Footer>
+    </CustomModal>
+  );
+};
---
+++ frontend/src/components/organisms/CreateCourseModal/useCreateCourseModal.ts
@@ -0,0 +1,58 @@
+import { useErrorHandler } from '@/hooks/useErrorHandler';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { CreateCourseData, createCourseSchema } from '@/schemas/course/createCourse';
+import { useCreateCourse } from '@/queries/course/mutation';
+import { useSnackbar } from 'notistack';
+import { invalidateCoursesCache } from '@/queries/course/invalidation';
+import { Course } from '@/schemas/course/course';
+
+export const useCreateCourseModal = ({
+  onClose,
+  onCreateSuccessCallback,
+}: {
+  onClose: () => void;
+  onCreateSuccessCallback?: (createdCourse: Course) => void;
+}) => {
+  const { mutateAsync: createCourse } = useCreateCourse();
+  const { enqueueSnackbar } = useSnackbar();
+  const { errorHandler } = useErrorHandler();
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<CreateCourseData>({
+    resolver: zodResolver(createCourseSchema),
+    defaultValues: {
+      name: '',
+      description: '',
+      start_date: new Date().toISOString(),
+      end_date: new Date().toISOString(),
+    },
+  });
+
+  const handleFormSubmit = async (data: CreateCourseData) => {
+    try {
+      const createdCourse = await createCourse(data);
+      invalidateCoursesCache();
+      reset();
+      onClose();
+      onCreateSuccessCallback?.(createdCourse);
+      enqueueSnackbar('Curso criado com sucesso', { variant: 'success' });
+    } catch (error) {
+      errorHandler(error);
+    }
+  };
+
+  return {
+    errors,
+    isSubmitting,
+    control,
+    register,
+    handleSubmit,
+    handleFormSubmit,
+  };
+};
